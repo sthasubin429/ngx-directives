162566c9e71e15b1a47999556df3733882a6d9a7
subin <48222247+sthasubin429@users.noreply.github.com>
2023-09-20 19:07:49 +0545

    feat: added tooltip directive

--- projects/ngx-directives/src/lib/ngx-directives.module.ts
+++ projects/ngx-directives/src/lib/ngx-directives.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { UnlessDirective } from './unless/unless.directive';
 import { HighlightDirective } from './highlight/highlight.directive';
+import { TooltipDirective } from './tooltip/tooltip.directive';
 
 @NgModule({
-  declarations: [UnlessDirective, HighlightDirective],
+  declarations: [UnlessDirective, HighlightDirective, TooltipDirective],
   imports: [],
-  exports: [UnlessDirective, HighlightDirective],
+  exports: [UnlessDirective, HighlightDirective, TooltipDirective],
 })
 export class NgxDirectivesModule {}
--- projects/ngx-directives/src/public-api.ts
+++ projects/ngx-directives/src/public-api.ts
@@ -5,3 +5,4 @@
 export * from './lib/ngx-directives.module';
 export * from './lib/unless/unless.directive';
 export * from './lib/highlight/highlight.directive';
+export * from './lib/tooltip/tooltip.directive';
